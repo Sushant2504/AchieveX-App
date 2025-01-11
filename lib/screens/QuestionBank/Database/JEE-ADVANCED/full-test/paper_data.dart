f015f26ae9abce61aec39f642ad43e2830fb7bd4
Sushant <sushantnadavade006@gmail.com>
2025-01-11 16:34:20 +0530

    question adding started

--- lib/screens/QuestionBank/Database/JEE-ADVANCED/full-test/paper_data.dart
+++ lib/screens/QuestionBank/Database/JEE-ADVANCED/full-test/paper_data.dart
@@ -1,8 +1,29 @@
 import 'package:flutter/material.dart';
 
+//format of the list
+//exam
+//subject
+//int questionNumber
+//question
+//year
+//bool: isquestionimage
+//string: questionimage
+//string: option1
+//string: option2
+//string: option3
+//string: option4
+//bool: isoptionimage
+//imagelink: option1
+//imagelink: option2
+//imagelink: option3
+//imagelink: option4
+//string or bool: answer
+//string + image: explanation
+//bool: isexplanationimage
+//String: image
 
 List<List<dynamic>> jeeadvanced_2024_paper1 = [
    [
-     
+      
    ],
 ];
